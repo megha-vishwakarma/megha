55d62e14bd13e22d6ab83f1aac1694838fe92810
megha-vishwakarma <70430389+megha-vishwakarma@users.noreply.github.com>
2024-10-29 14:19:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+// let tablinks = document.getElementsByClassName("tab-links");
+// let tabcontents = document.getElementsByClassName("tab-contents");
+
+// function opentab(tabname) {
+//     for (tablink of tablinks) {
+//         tablink.classList.remove("active-link")
+//     }
+//     for (tabcontent of tabcontents) {
+//         tabcontent.classList.remove("active-tab")
+//     }
+//     event.currentTarget.classList.add("active-link");
+//     document.getElementById(tabname).classList.add("active-tab")
+// }
+
+// const sidemenu = document.getElementById("sidemenu");
+
+// function openMenu() {
+//     sidemenu.style.right = '0'
+// }
+
+// function closeMenu() {
+//     sidemenu.style.right = '-200vw'
+// }
+
+
